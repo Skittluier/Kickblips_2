da7885e87e1356446e1a994bd17f9ace0ccee26a
Maarten Braaksma <maarten@bucketheadentertainment.com>
2024-02-20 23:00:10 +0100

    Iterate UI
    - The instruction item sprites will now be changed whenever a device change has happened.

--- Assets/Scripts/UI/InputToImageConverter.cs
+++ Assets/Scripts/UI/InputToImageConverter.cs
@@ -1,12 +1,16 @@
 namespace KickblipsTwo.UI
 {
+    using KickblipsTwo.Managers;
     using UnityEngine;
+    using UnityEngine.InputSystem;
     using UnityEngine.UI;
 
     public class InputToImageConverter : MonoBehaviour
     {
         [SerializeField, Tooltip("The input that should be converted to a sprite. Empty means that nothing will be shown. (programmer will update this then)")]
         private KickblipsTwo.Input.Input input;
+
+        private InputDevice currentlyKnownInputDevice;
 
         [SerializeField, Tooltip("The reference to the UI image component.")]
         private Image inputImage;
@@ -19,10 +23,22 @@ namespace KickblipsTwo.UI
         }
 
         /// <summary>
+        /// Checking every frame whether the input device has changed. If thats the case, then update the button sprite.
+        /// </summary>
+        private void Update()
+        {
+            if (InputManager.CurrentlyUsedDevice != currentlyKnownInputDevice)
+                UpdateButtonSprite(input);
+        }
+
+        /// <summary>
         /// Translates the current device input into a sprite.
         /// </summary>
         internal void UpdateButtonSprite(KickblipsTwo.Input.Input input)
         {
+            currentlyKnownInputDevice = InputManager.CurrentlyUsedDevice;
+
+            this.input = input;
             inputImage.sprite = input.GetDeviceVisualInput().inputSprite;
         }
     }
